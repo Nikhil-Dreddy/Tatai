ee90890fc2adbc700b15b7622dde5850db7161d2
eskit <nikhilreddy24@hotmail.com>
2017-09-20 12:57:09 +1200

    Changed the scence changes to the correct one

---
+++ A03/src/application/view/MenuController.java
@@ -0,0 +1,55 @@
+package application.view;
+
+import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.model.NumberGenerator;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class MenuController implements Initializable{
+	NumberGenerator number = new NumberGenerator();
+	@FXML
+	Label a;
+	
+	public void easyButtonClicked(ActionEvent event) throws IOException{
+		number.setDifficultyToEasy();
+		changeSceneToRecord(event);
+	}
+	
+
+	public void hardButtonClicked(ActionEvent event) throws IOException{
+		number.setDifficultyToHard();
+		changeSceneToRecord(event);
+	}
+	
+	// Change scene to record scene when button is pushed
+	public void changeSceneToRecord(ActionEvent event) throws IOException{
+		Parent recordViewParent;
+		
+		recordViewParent = FXMLLoader.load(getClass().getResource("Record.fxml"));
+		Scene recordViewScene = new Scene(recordViewParent);
+		
+//		Get the stage
+		Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		
+		stage.setScene(recordViewScene);
+		stage.show();
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ A03/src/application/view/RecordController.java
@@ -0,0 +1,59 @@
+package application.view;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.model.NumberGenerator;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class RecordController implements Initializable{
+	
+	NumberGenerator numberGenerator = new NumberGenerator();
+	
+	private static int questionNo = 0;
+	
+	private static int score;
+	private static int mistakes;
+	
+	
+	@FXML
+	private Label numLabel;
+	@FXML
+	private Label questionLabel;
+	
+	public void setNumLabel(){
+		numberGenerator.generateNum();
+		numLabel.setText(String.valueOf(numberGenerator.getNum()));
+		questionNo++;
+		if(questionNo == 10 ){
+			questionNo = 0;
+			
+		}
+		questionLabel.setText(questionNo +"/10");	
+		
+	}
+	
+	public void changeSceneToMenu(ActionEvent event) throws IOException{
+		Parent menuViewParent = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+		Scene menuViewScene = new Scene(menuViewParent);
+		Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		stage.setScene(menuViewScene);
+		stage.show();
+	}
+	
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		setNumLabel();
+	}
+
+}
