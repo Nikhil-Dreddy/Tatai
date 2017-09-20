e7b998f950440f6842df1fd89111237db11186cf
Clout-God <nikhilreddy24@hotmail.com>
2017-09-20 21:26:44 +1200

    added the HTK files & SpeechScript Class

---
+++ A03/src/application/model/SpeechScript.java
@@ -0,0 +1,13 @@
+package application.model;
+
+import javafx.concurrent.Task;
+
+public class SpeechScript extends Task<Void> {
+
+	@Override
+	protected Void call() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+ 
+}
