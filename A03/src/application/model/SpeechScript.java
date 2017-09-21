1096e85f10ecce3acb31b02b71999edc9db934f1
Young Sun An <yan653@en-337200.uoa.auckland.ac.nz>
2017-09-22 11:47:37 +1200

    dekleted main

--- A03/src/application/model/SpeechScript.java
+++ A03/src/application/model/SpeechScript.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-import application.Main;
+import application.MainApp;
 import application.view.RecordController;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
