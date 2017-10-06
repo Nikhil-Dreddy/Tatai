ac19428bb6bb8bc7d93b1917f86c35c3d0c25fbb
Youngsun An <Youngsun@wifi-172-23-3-208.uoa-wifi.auckland.ac.nz>
2017-10-06 22:34:57 +1300

    yoza comment

--- src/application/MainApp.java
+++ src/application/MainApp.java
@@ -13,7 +13,7 @@ import javafx.scene.Scene;
 public class MainApp extends Application {
 
 	private Stage primaryStage;
-	// hello very much
+	// yoza
 
 	@Override
 	public void start(Stage primaryStage) throws IOException, InterruptedException {
