dc9398f7ab94585c716d65529e88cabfbe953f91
Youngsun An <Youngsun@wifi-172-23-3-208.uoa-wifi.auckland.ac.nz>
2017-10-06 22:04:52 +1300

    more crap  massage

--- src/application/MainApp.java
+++ src/application/MainApp.java
@@ -13,7 +13,7 @@ import javafx.scene.Scene;
 public class MainApp extends Application {
 
 	private Stage primaryStage;
-	// hi
+	// hello very much
 
 	@Override
 	public void start(Stage primaryStage) throws IOException, InterruptedException {
